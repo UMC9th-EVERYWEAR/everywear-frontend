d1d1f75e35ad71f47ba6317a81fc668584d54e7f
Cuyqo <saon0410@gmail.com>
2026-01-06 10:41:34 +0900

    chore: configure ESLint and Prettier

--- eslint.config.js
+++ eslint.config.js
@@ -3,7 +3,14 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
+import jsxA11y from 'eslint-plugin-jsx-a11y'
+import importPlugin from 'eslint-plugin-import'
+import eslintPlugin from '@typescript-eslint/eslint-plugin'
 import { defineConfig, globalIgnores } from 'eslint/config'
+import react from 'eslint-plugin-react'
+
+import eslintConfigPrettier from 'eslint-config-prettier'; 
+
 
 export default defineConfig([
   globalIgnores(['dist']),
@@ -14,10 +21,55 @@ export default defineConfig([
       tseslint.configs.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
+      eslintConfigPrettier,
+      react.configs.flat.recommended,
+
     ],
     languageOptions: {
-      ecmaVersion: 2020,
+      ecmaVersion: 2020,     
+      ...react.configs.flat.recommended.languageOptions,
+
       globals: globals.browser,
+    }, 
+    plugins: {
+      'react': react,
+      'react-hooks': reactHooks,
+      'jsx-a11y': jsxA11y,
+      'import': importPlugin,
     },
+    rules: {
+          ...js.configs.recommended.rules,
+          ...eslintPlugin.configs.recommended.rules,
+          ...react.configs.recommended.rules,
+          ...reactHooks.configs.recommended.rules,
+            ...jsxA11y.configs.recommended.rules,
+          'quotes': ['error', 'single'],
+          "no-unused-vars": "off",
+
+          'react/react-in-jsx-scope': 'off', 
+          'react-refresh/only-export-components': [
+            'warn',
+            { allowConstantExport: true },
+          ],
+          'indent': ['warn', 'tab', {
+            'SwitchCase': 2,
+            'ignoredNodes': ['TemplateLiteral'],
+          }],
+          'react/jsx-indent': ['warn', 'tab'], 
+                    'comma-dangle': ['warn', 'always-multiline'],
+
+          'react/jsx-indent-props': ['warn', 'tab'],
+          'react/jsx-first-prop-new-line': ['warn', 'multiline'],
+          'react/jsx-closing-bracket-location': ['warn', 'tag-aligned'],
+          'react/jsx-max-props-per-line': ['warn', { maximum: 1 }],
+          'react/jsx-first-prop-new-line': ['warn', 'multiline'],
+          'react/jsx-closing-bracket-location': ['warn', 'tag-aligned'],
+
+          'object-curly-spacing': ['warn', 'always'],
+          'array-bracket-spacing': ['warn', 'never'],
+          'comma-dangle': ['warn', 'always-multiline'],
+          'eol-last': ['warn', 'always'],
+        },
+    
   },
 ])
