adafe18f59c32f94f6e0ff147cabcd95880f55a8
Cuyqo <saon0410@gmail.com>
2026-01-15 02:14:55 +0900

    fix: change navbar position from absolute to sticky

--- src/App.tsx
+++ src/App.tsx
@@ -6,14 +6,6 @@ function App() {
 
 	return ( 
 		<RouterProvider router={router} />
-    
-	// <div className="min-h-screen bg-gray-50 flex justify-center">
-	// 	<div className="relative w-[375px] h-screen bg-white shadow-lg overflow-hidden flex flex-col">
-        
-
-
-	// 	</div>
-	// </div>
 	);
 }
 
