e8e8286051b6c7551316e180b14182c58913288f
Cuyqo <saon0410@gmail.com>
2026-01-16 22:16:03 +0900

    feat: implement setting photo change page UI

---
+++ src/vite-env.d.ts
@@ -0,0 +1,9 @@
+//  typescript가 환경변수를 추론하게 하기 위한 파일
+/// <reference types="vite/client" />
+
+declare module '*.css';
+declare module '*.scss';
+declare module '*.sass';
+declare module 'swiper/css';
+declare module 'swiper/css/pagination';
+declare module 'swiper/css/navigation';
