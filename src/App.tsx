f149e63c006699e83f1ab8ec80df8491daf375e1
김재범 <process68@naver.com>
2026-01-20 11:52:07 +0900

    feat: configure App routing to render Home via ProtectedRoute

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import ProtectedRoute from './router/routes/ProtectedRoute'; // ✅ 알려주신 경로 반영
+import ProtectedRoute from './router/routes/ProtectedRoute'; 
 import Home from './pages/Home';
 
 function App() {
@@ -10,13 +10,8 @@ function App() {
         <div className="relative w-[375px] h-screen bg-white shadow-lg overflow-hidden flex flex-col">
           
           <Routes>
-            {/* 🔒 레이아웃(헤더/네브바)이 포함된 ProtectedRoute를 부모로 설정 */}
             <Route element={<ProtectedRoute />}>
-              {/* ✅ "/" 경로로 들어오면 바로 Home 컴포넌트를 보여줌 */}
               <Route path="/" element={<Home />} />
-              
-              {/* 나중에 페이지가 추가되면 여기에 더 넣으시면 됩니다 */}
-              {/* <Route path="/setting" element={<SettingPage />} /> */}
             </Route>
           </Routes>
 
