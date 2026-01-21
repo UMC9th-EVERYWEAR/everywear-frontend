d3d0b42cb30c64d31aaa0366bc11472db0f5bd53
brayden1102 <eyssik05@naver.com>
2026-01-21 22:22:57 +0900

    chore: add clsx library

--- src/pages/ai-fitting/AiFittingPage.tsx
+++ src/pages/ai-fitting/AiFittingPage.tsx
@@ -1,7 +1,23 @@
+import { useState } from 'react';
+
 const AiFittingPage = () => {
+	const [isAiFitting, setIsAiFitting] = useState(0);
+	const tabBarCss = ['text-primary-600 border-b border-b-1.5 border-b-primary-600', 'text-neutral-400 border-b border-b-0.5 border-b-neutral-400'];
+	const handleTabBar = () => {
+		setIsAiFitting((prev) => prev ? 0 : 1)
+	}
 	return (
-		<div>
-			AiFittingPage
+		<div className="flex flex-col">
+			<div className="flex max-w-2xs transition-all duration-300 h-5">
+				<button
+					className={}
+					onClick={handleTabBar}
+				>AI 피팅</button>
+				<button
+					className={`${tabBarCss[1 - isAiFitting]} text-base`}
+					onClick={handleTabBar}
+				>AI 리뷰</button>
+			</div>
 		</div>
 	)
 }
