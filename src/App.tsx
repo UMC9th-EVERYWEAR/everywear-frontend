6fdbeddb8ee9aec1c1bfe192878f92e9ce0c38c2
김재범 <process68@naver.com>
2026-01-22 13:09:29 +0900

    feat: refactor auth router and add fitting pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,10 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import { PATH } from './router/path'; 
-import RootLayout from '@/src/components/layout/RootLayout';
-import ProtectedRoute from '@/src/router/routes/ProtectedRoute';
-import Home from '@/src/pages/Home';
+import { RouterProvider } from 'react-router';
+import { router } from './router/router';
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route element={<RootLayout />}>
-          <Route element={<ProtectedRoute />}>
-            <Route path="/" element={<Navigate to={PATH.HOME} replace />} />
-            
-            <Route path={PATH.HOME} element={<Home />} />
-          </Route>
 
-        </Route>
-      </Routes>
-    </BrowserRouter>
+  return (
+    <RouterProvider router={router} />
   );
 }
 
