854ba6b145490bde5297e86a8ff15d7404bf04b3
brayden1102 <eyssik05@naver.com>
2026-01-24 15:12:54 +0900

    chore: add Anonymous-Pro font

--- src/pages/closet/ClosetPage.tsx
+++ src/pages/closet/ClosetPage.tsx
@@ -1,10 +1,16 @@
+import CategoryBar from '@/src/components/common/CategoryBar';
 
 const ClosetPage = () => {
 	return (
-		<div>
+		<div className="flex flex-col items-center">
+			{/* 카테고리 바 */}
+			<CategoryBar />
+			<div className='flex justify-between'>
+				<p className='text-regular-14'></p>
+			</div>
       
 		</div>
 	)
 }
 
-export default ClosetPage
+export default ClosetPage;
