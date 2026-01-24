a888a3a0b7cb8554759ff6cc1cd957484edbac39
Cuyqo <saon0410@gmail.com>
2026-01-25 02:59:36 +0900

    feat: add onboarding notice section

---
+++ src/components/onboarding/OnboardingNotice.tsx
@@ -0,0 +1,60 @@
+import accuracy from '@/public/svgs/onboarding/accuracy-icon.svg'
+import security from '@/public/svgs/onboarding/lock-icon.svg'
+import speed from '@/public/svgs/onboarding/cart-icon.svg'
+
+
+type FeatureKey = 'ACCURACY' | 'SECURITY' | 'SPEED';
+
+
+interface OnboardingNoticeProps {
+feature: FeatureKey
+}
+
+const FEATURE_CONFIG: Record<
+  FeatureKey,
+  {
+    title: string;
+    description: string;
+    icon: string;
+  }
+> = {
+	ACCURACY: {
+		title: '정확한 AI 피팅',
+		description: '정면 전신 사진으로 더 정확한 피팅 결과를 제공합니다.',
+		icon: accuracy,
+	},
+	SECURITY: {
+		title: '안전한 보관',
+		description: '사진은 안전하게 암호화되어 보관되며, 본인만 확인 가능합니다.',
+		icon: security,
+	},
+	SPEED: {
+		title: '빠른 피팅',
+		description: '한 번 등록으로 모든 상품을 빠르게 가상으로 착용해보세요.',
+		icon: speed,
+	},
+};
+
+const OnboardingNotice = ({ feature }: OnboardingNoticeProps) => {
+	const { title, description, icon } = FEATURE_CONFIG[feature];
+
+	return (
+		<div className="flex gap-4 rounded-xl bg-white p-4 items-center max-w-109">
+
+			<div className='w-11 h-11 min-w-11 rounded-full bg-neutral-200 flex justify-center items-center'>
+				<img
+					src={icon}
+					alt={title}
+					className="h-6 w-6"
+				/>
+			</div>
+
+			<div>
+				<h3 className="text-bold-18 text-primary-600">{title}</h3>
+				<p className="mt-1 text-sm text-gray-600">{description}</p>
+			</div>
+		</div>
+	);
+};
+
+export default OnboardingNotice;
--- src/pages/onboarding/onboarding-page.tsx
+++ src/pages/onboarding/onboarding-page.tsx
@@ -1,8 +1,38 @@
 import camera from '@/public/svgs/onboarding/camera-blue.svg'
+import OnboardingNotice from '@/src/components/onboarding/OnboardingNotice'
+import OnboardingTip from '@/src/components/onboarding/OnboardingTip'
 const OnBoardingPage = () => {
-	return <div className='bg-bg-onboarding'><img
-		src={camera}
-		alt='camera'
-	                                         /></div>
+	return <div className='bg-onboarding min-h-screen pt-20 px-5'>
+		
+		<div className='flex flex-col items-center '>
+
+			<img
+				src={camera}
+				alt='camera'
+				width={55}
+			/>
+
+			<div className='mt-7 text-semibold-24 text-center'>
+				<p className=''>가상 피팅을 위한</p>				
+				<p className=''>사진을 등록해주세요</p>		
+			</div>
+
+			<div className='text-center mt-4'>
+				{/*TODO: Anonymous Pro 폰트 수정*/}
+				<p>업로드하신 사진은 AI 가상 피팅에 사용됩니다.</p>
+				<p> 가이드를 참고하여 정확한 피팅 결과를 받아보세요!</p>										 											 
+			</div>
+
+			<div className='flex flex-col gap-6 mt-5 mb-9'>
+				<OnboardingNotice feature='ACCURACY'/>
+				<OnboardingNotice feature='SECURITY'/>
+				<OnboardingNotice feature='SPEED'/>
+			</div>
+
+			<OnboardingTip />
+			<div>
+			</div>
+		</div>
+	</div>
 }
 export default OnBoardingPage
