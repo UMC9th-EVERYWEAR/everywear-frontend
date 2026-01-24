ce21b7f44e95bd025393dea999b9d99d861647bd
brayden1102 <eyssik05@naver.com>
2026-01-24 16:49:31 +0900

    feat: complete ClosetPage ui

--- src/pages/closet/ClosetPage.tsx
+++ src/pages/closet/ClosetPage.tsx
@@ -7,12 +7,13 @@ import { useNavigate } from 'react-router';
 const ClosetPage = () => {
 	const navigate = useNavigate();
 
+
 	return (
 		<div className="flex flex-col items-center">
 			<CategoryBar />
 			<div className='w-[375px] flex flex-col mb-5'> {/* 나중에 width 설정 바꿔야 함 */}
 				{/* 상품 종류 및 개수 & 상품 추가 버튼 */}
-				<div className='flex justify-between items-center'>
+				<div className='flex justify-between items-center mb-5'>
 					<p className='text-regular-14 font-anonymous'>상의 상품(3)</p>
 					<button
 						className='w-[70.667px] h-6 px-1.75 py-1.25 flex justify-center 
@@ -23,7 +24,7 @@ const ClosetPage = () => {
 				</div>
 
 				{/* 내 상품 본문 */}
-				<div className=''>
+				<div className='grid grid-cols-2 gap-4 place-items-center'>
 					<ProductCard
 						company='무신사'
 						name='베이직 화이트 셔츠'
@@ -31,6 +32,35 @@ const ClosetPage = () => {
 						rating={4.7}
 						imageUrl='https://lh3.googleusercontent.com/d/1XuItc3eisxkLo6ZXqClQs-ZcsbYU0brI'
 					/>
+					<ProductCard
+						company='무신사'
+						name='베이직 화이트 셔츠'
+						price='29,000'
+						rating={4.7}
+						imageUrl='https://lh3.googleusercontent.com/d/1XuItc3eisxkLo6ZXqClQs-ZcsbYU0brI'
+					/>
+					<ProductCard
+						company='무신사'
+						name='베이직 화이트 셔츠'
+						price='29000'
+						rating={4.7}
+						imageUrl='https://lh3.googleusercontent.com/d/1XuItc3eisxkLo6ZXqClQs-ZcsbYU0brI'
+					/>
+					<ProductCard
+						company='무신사'
+						name='베이직 화이트 셔츠'
+						price='29000'
+						rating={4.7}
+						imageUrl='https://lh3.googleusercontent.com/d/1XuItc3eisxkLo6ZXqClQs-ZcsbYU0brI'
+					/>
+					<ProductCard
+						company='무신사'
+						name='베이직 화이트 셔츠'
+						price='29000'
+						rating={4.7}
+						imageUrl='https://lh3.googleusercontent.com/d/1XuItc3eisxkLo6ZXqClQs-ZcsbYU0brI'
+					/>
+					
 				</div>
 
 
