d98f6d6d4de8d54aecc5e97473fbdc9386a95262
Cuyqo <saon0410@gmail.com>
2026-01-25 21:00:31 +0900

    feat: add camera btn ui

--- src/components/onboarding/onboarding-photo/AddPhotoSection.tsx
+++ src/components/onboarding/onboarding-photo/AddPhotoSection.tsx
@@ -17,12 +17,9 @@ const AddPhotoSection = ({ setShowGuide } : AddPhotoSectionProps) => {
 	const videoRef = useRef<HTMLVideoElement | null>(null);
 	const canvasRef = useRef<HTMLCanvasElement | null>(null);
 
-	
-	
 	const handleClick = async (btnType: PhotoBtnType) => {
 		if (btnType === 'CAMERA') {
 			if (!videoRef.current) return;
-			console.log('ㅎㅇ')
 			const stream = await getWebcamStream();
 			// camera 처리
 			videoRef.current.srcObject = stream
@@ -34,30 +31,30 @@ const AddPhotoSection = ({ setShowGuide } : AddPhotoSectionProps) => {
 		}
 	};
 
-	const drawToCanvas = () => {
-		try {
-			const ctx = canvasRef.current?.getContext('2d');
-			if(!canvasRef.current) return;
+	// 화질 안좋음 이슈 함수
+	// const drawToCanvas = () => {
+	// 	try {
+	// 		const ctx = canvasRef.current?.getContext('2d');
+	// 		if(!canvasRef.current) return;
 
-			canvasRef.current.width = videoRef?.current.width
-			canvasRef.current.height = 100
-			if(ctx && ctx !== null) {
-				if(videoRef.current){
-					ctx.translate(canvasRef.current.width, 0);
-					ctx.scale(-1, 1);
-					ctx.drawImage(videoRef.current, 0, 0, canvasRef.current.width, canvasRef.current.height)
-					ctx.setTransform(1,0,0,1,0,0);
+	// 		canvasRef.current.width = videoRef?.current.width
+	// 		canvasRef.current.height = 100
+	// 		if(ctx && ctx !== null) {
+	// 			if(videoRef.current){
+	// 				ctx.translate(canvasRef.current.width, 0);
+	// 				ctx.scale(-1, 1);
+	// 				ctx.drawImage(videoRef.current, 0, 0, canvasRef.current.width, canvasRef.current.height)
+	// 				ctx.setTransform(1,0,0,1,0,0);
 
-				}
-			}
-		}
-		 catch (err){
-			console.log(err)
-		}
-	}
+	// 			}
+	// 		}
+	// 	}
+	// 	 catch (err){
+	// 		console.log(err)
+	// 	}
+	// }
 	
 	  const handleCapture = () => {
-		console.log('찍는다')
 		if (!videoRef.current || !canvasRef.current) {
 			return;
 		}
@@ -76,6 +73,7 @@ const AddPhotoSection = ({ setShowGuide } : AddPhotoSectionProps) => {
 		context.drawImage($video, 0, 0, $canvas.width, $canvas.height);
 		const imageToDataUrl = $canvas.toDataURL('image/png');
 		setPhoto(imageToDataUrl);
+		setIsCamera(false)
 	};
  
 
@@ -85,20 +83,27 @@ const AddPhotoSection = ({ setShowGuide } : AddPhotoSectionProps) => {
 		<>
 			{
 				!isCamera && 
-				<div className="px-5 pt-4 flex justify-center flex-col items-center">
+				<div className="px-5 p-4 flex justify-center flex-col items-center">
 					<div className='flex flex-col gap-4'>
-						<div className="border w-85 border-neutral-200  rounded-sm px-8 py-5 flex flex-col items-center mb-5.5">
-							<p className='text-neutral-500 text-regular-12 text-end w-full mb-3'><button
+						<div className="border w-85 border-neutral-200  rounded-sm px-8 py-5 flex flex-col items-center">
+							<p className='text-neutral-500 text-regular-12 text-end w-full'><button
 								onClick={setShowGuide}
 								className='hover:underline cursor-pointer'
-							                                                                     >
+							                                                                >
 								사진 등록 가이드
 							</button>
 							</p>
 							<p className='text-neutral-900 mb-2.5'>한명만 나온 사진을 등록해주세요!</p>
 
-							<div className="border-dashed border w-full border-neutral-500 rounded-lg h-84 bg-onboarding-photo flex justify-center items-center text-neutral-600">
+							<div className="border-dashed border w-full border-neutral-500 rounded-lg h-84 bg-onboarding-photo flex justify-center items-center text-neutral-600 overflow-hidden">
 								{/* 사진을 추가해주세요! 등 멘트 수정 필요해보임 */}
+								{photo && 
+								<img
+									src={photo} 
+									alt='fitting-photo'
+									className='w-full h-full border object-cover'
+								/>
+								}
 							</div>
 						</div>
 
@@ -112,12 +117,30 @@ const AddPhotoSection = ({ setShowGuide } : AddPhotoSectionProps) => {
 								handleClick={handleClick}
 							/>
 						</div>
-						<Button disabled={!hasFile}>확인하기</Button>
-						<button
+						<Button disabled={!photo}>확인하기</Button>
+
+						{/*btn 디자인 후보들 */}
+
+						{/*btn1 */}
+						{/* <div className='absolute bottom-0 -mx-5 bg-white w-full h-30 flex justify-center items-center '>
+							<button
+								onClick={handleCapture}
+								className='bg-primary-600 rounded-full w-15 h-15 flex justify-center items-center'
+							>
+								<div
+									className='bg-primary-600 border-white border-[1.5px] rounded-full w-13 h-13'
+								></div>
+							</button>
+						</div> */}
+						{/*btn2 */}
+						{/* <button
 							onClick={handleCapture}
-							className='  bg-neutral-500 rounded-full w-6 h-6'
-						></button>
-	
+							className='absolute bottom-20 left-1/2 -translate-x-1/2 bg-white rounded-full w-15 h-15 flex justify-center items-center'
+						>
+							<div
+								className='bg-white border-primary-600 border-[1.5px] rounded-full w-13 h-13'
+							></div>
+						</button> */}
 
 					</div>
 				</div>
@@ -125,8 +148,8 @@ const AddPhotoSection = ({ setShowGuide } : AddPhotoSectionProps) => {
 
 
 			<div
-				className={cn('min-h-screen relative',
-					isCamera ? '' : 'invisible',
+				className={cn('relative',
+					isCamera ? 'min-h-screen h-full' : 'invisible h-0',
 				)}
 			>
 				{ }
@@ -134,27 +157,25 @@ const AddPhotoSection = ({ setShowGuide } : AddPhotoSectionProps) => {
 					ref={videoRef}
 					autoPlay
 					playsInline
-					  className=" w-screen min-h-screen border	border-primary-600 h-full object-cover"
+					  className={cn('w-screen border border-primary-600 object-cover',
+						isCamera ? 'min-h-screen h-full' : 'invisible h-0',
+
+					)}
 					muted // ios
 				/>
+				{/*btn2 */}
 				<button
 					onClick={handleCapture}
-					className='absolute top-10 bg-neutral-500'
-				>버튼입니당</button>
-	
+					className='absolute bottom-20 left-1/2 -translate-x-1/2 bg-white rounded-full w-15 h-15 flex justify-center items-center cursor-pointer'
+				>
+					<div
+						className='bg-white border-primary-600 border-[1.5px] rounded-full w-13 h-13'
+					></div>
+				</button>
 				      <canvas
 					ref={canvasRef}
+					className="hidden"
 				      />
-
-				   {photo && (
-					<div>
-						<h1>캡쳐된 이미지</h1>
-						<img
-							src={photo}
-							alt='Captured'
-						/>
-					</div>
-				)}
 			</div>
 		
 		</>
