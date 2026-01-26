03e8ab3ec635e225e5c5827d47aece937534de5d
kimjaebeom <process6870@gmail.com>
2026-01-27 04:05:05 +0900

    feat: icon-system-images

--- src/components/common/Icons.tsx
+++ src/components/common/Icons.tsx
@@ -4,36 +4,46 @@ import React from 'react';
 
 interface IconProps extends React.SVGProps<SVGSVGElement> {
   size?: number;
+  height?: number;
+  active?: boolean; // 활성화 여부
+  className?: string;
 }
 
-export const HomeIcon = ({ size = 16, height = 15.75, ...props }: IconProps) => (
-	<ICON_DATA.HomeIcon
-		width={size}
-		height={height}
-		{...props}
-	/>
+export const HomeIcon = ({ size = 24, height = 24, active = false, ...props }: IconProps) => (
+  <img 
+    src={ICON_DATA.HomeIcon(active)} 
+    width={size} 
+    height={height} 
+    alt="홈 아이콘"
+    {...props} 
+  />
+);
+export const ProductIcon = ({ size = 24, height = 24, active = false, ...props }: IconProps) => (
+	<img 
+    src={ICON_DATA.ProductIcon(active)} 
+    width={size} 
+    height={height} 
+    alt="홈 아이콘"
+    {...props} 
+  />
 );
 
-export const ProductIcon = ({ size = 18, height = 16, ...props }: IconProps) => (
-	<ICON_DATA.ProductIcon
-		width={size}
-		height={height}
-		{...props}
-	/>
+export const FittingIcon = ({ size = 24, height = 24, active = false,...props }: IconProps) => (
+	<img 
+    src={ICON_DATA.FittingIcon(active)} 
+    width={size} 
+    height={height} 
+    alt="홈 아이콘"
+    {...props} 
+  />
 );
 
-export const FittingIcon = ({ size = 19.998, height = 16, ...props }: IconProps) => (
-	<ICON_DATA.FittingIcon
-		width={size}
-		height={height}
-		{...props}
-	/>
-);
-
-export const ClosetIcon = ({ size = 13.412, height = 15.85, ...props }: IconProps) => (
-	<ICON_DATA.ClosetIcon
-		width={size}
-		height={height}
-		{...props}
-	/>
+export const ClosetIcon = ({ size = 24, height = 24, active = false,...props }: IconProps) => (
+	<img 
+    src={ICON_DATA.ClosetIcon(active)} 
+    width={size} 
+    height={height} 
+    alt=" 아이콘"
+    {...props} 
+  />
 );
