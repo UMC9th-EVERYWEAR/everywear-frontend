2dc27d19127b5f3917a4506c2a11c0ea25dda94e
Cuyqo <saon0410@gmail.com>
2026-01-27 22:10:13 +0900

    feat: add constants mall_links

---
+++ src/constants/link.ts
@@ -0,0 +1,24 @@
+
+export const MALL_LINKS = {
+	ZIGZAG:
+	{
+		label: '지그재그',
+		url: 'https://zigzag.onelink.me/9c0A',
+	},
+	MUSINSA:
+	{
+		label: '무신사',
+		url: 'https://musinsa.onelink.me/PvkC',
+	},
+	CM:
+	{
+		label: '29CM',
+		url: 'https://29cm.onelink.me/0M6z',
+	},
+	WCONCEPT:
+	{
+		label: 'W컨셉',
+		url: 'https://wconcept.onelink.me/3h7P',
+	},
+} as const;
+
