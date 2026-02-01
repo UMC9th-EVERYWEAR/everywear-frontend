4cecad83d042e0e6aa4c2621897ca5281aa68c53
brayden1102 <eyssik05@naver.com>
2026-02-01 19:28:57 +0900

    docs: add annotaion in apiClient

--- src/apis/common/apiClient.ts
+++ src/apis/common/apiClient.ts
@@ -2,6 +2,12 @@ import { ENV_CONFIG } from '@/src/constants/config';
 import { createStorage, STORAGE_KEY, TOKEN_STORAGE_TYPE } from '@/src/utils/useStorage';
 import axios, { type InternalAxiosRequestConfig } from 'axios';
 
+// 백엔드에서 내려주는 액세스 토큰 데이터 형식
+interface RefreshResponse {
+    result: {
+        accessToken: string;
+    };
+}
 
 interface CustomInternalAxiosRequestConfig extends InternalAxiosRequestConfig {
     _retry? : boolean; // 요청 재시도 여부를 나타내는 플래그
@@ -25,6 +31,11 @@ const accessTokenStorage = createStorage<string>(
 	TOKEN_STORAGE_TYPE,
 );
 
+// axiosInstance에 토큰을 처음부터 주입하지 않음.
+// 주입하지 않는 이유
+// 1. 앱이 처음 켜질 때는 토큰이 없는데 axiosInstance에 토큰을 넣어놓을 경우 Bearer : null로 설정
+//    그러면 로그인을 해도 실제 액세스 토큰으로 바뀌지 않음.
+// 2. 토큰이 갱신되어도 옛날 토큰을 계속 사용함.
 export const axiosInstance = axios.create({
 	// 모든 요청의 기본 주소 설정
 	// https://api.myserver.com/v1/users에서 https://api.myserver.com를 의미
@@ -34,6 +45,11 @@ export const axiosInstance = axios.create({
 	headers: {
 		'Content-Type' : 'application/json',
 	},
+	// 5초 동안 응답이 없으면 에러 발생 (error code : ECONNABORTED)
+	timeout: 5000, 
+	// // 예: 파일 업로드 시에는 30초로 연장
+	// axiosInstance.post('/upload', formData, { timeout: 30000 });
+
 	// 백엔드에서 리프레시토큰을 쿠키에 담아서 관리하므로 
 	// withCredentials = true로 설정하여 브라우저가 쿠키를 포함시켜서 서버로 가져감
 	withCredentials: true, 
@@ -41,13 +57,14 @@ export const axiosInstance = axios.create({
 
 axiosInstance.interceptors.request.use(
 	(config) => {
-		// 리프레시 요청에는 Autorization 헤더를 넣지 않도록 통제
+		// 리프레시 요청에는 Autorization 헤더를 넣지 않도록 제어 => 헤더 제어
 		if (config.url?.includes('/api/auth/refresh')) {
 			return config;
 		}
 
 		const accessToken = accessTokenStorage.getItem();
 
+		// AccessToken이 있을 때만 헤더 주입
 		if (accessToken) {
 			config.headers.Authorization = `Bearer ${accessToken}`;
 		}
@@ -72,6 +89,13 @@ axiosInstance.interceptors.response.use(
 			return Promise.reject(error);
 		}
 
+		// 타임아웃 시 에러 처리
+		// TODO : 백엔드에서 보내주는 에러메시지 확인 후 수정 필요
+		// TODO : 필요하다면 타임아웃 시 토스트 띄우기 등 추가 필요
+		if (error.code === 'ECONNABORTED' || error.message.includes('timeout')) {
+			console.error('요청 시간이 초과되었습니다.');
+			return Promise.reject(error);
+		}
 		console.log('401 에러 감지: 토큰 갱신 시도');
 		originalRequest._retry = true; // 재시도 플래그 설정
 
@@ -79,7 +103,8 @@ axiosInstance.interceptors.response.use(
 			// 갱신 요청 로직 시작
 			if (!refreshPromise) {
 				refreshPromise = (async () => {
-					const { data } = await axiosInstance.post('/api/auth/refresh');
+					// 제네릭으로 타입 명시 <- data 타입이 명시되지 않아 any로 추론될 수 있음.
+					const { data } = await axiosInstance.post<RefreshResponse>('/api/auth/refresh');
 
 					const newAccessToken = data.result.accessToken;
 					accessTokenStorage.setItem(newAccessToken);
@@ -100,6 +125,7 @@ axiosInstance.interceptors.response.use(
       
 			return axiosInstance(originalRequest);
 
+			// 갱신 실패시 스토리지 비우기 및 로그아웃 처리
 		} catch (refreshError) {
 			console.error('토큰 갱신 실패:', refreshError);
 			accessTokenStorage.removeItem();
