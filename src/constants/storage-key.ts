c2d37f65f6aa99961ccc063a27a6d12f97735a72
brayden1102 <eyssik05@naver.com>
2026-02-01 17:54:11 +0900

    feat: add interface of local & session storage

---
+++ src/constants/storage-key.ts
@@ -0,0 +1,9 @@
+export const LOCAL_STORAGE_KEY = {
+	accessToken: 'accessToken',
+	refreshToken: 'refreshToken',
+};
+
+export const SESSION_STORAGE_KEY = {
+	accessToken: 'accessToken',
+	refreshToken: 'refreshToken',
+}
