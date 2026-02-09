21fb04260954df9ad810af1052d8f25f38359e7d
brayden1102 <eyssik05@naver.com>
2026-02-09 13:26:32 +0900

    feat: add timeSet file & apply in fitting.ts

--- src/apis/domain/fitting.ts
+++ src/apis/domain/fitting.ts
@@ -1,9 +1,10 @@
 import { apiClient } from '@/src/apis/common/apiClient';
 import type { FittingRequest } from '../generated';
+import { fittingTimeSet } from '@/src/constants/timeSet';
 
 export const requestFitting = async (payload: FittingRequest) => {
 	// 타임아웃 방지용으로 시간 5분으로 설정
-	const { data } = await apiClient.requestFitting(payload, { timeout: 300000 });
+	const { data } = await apiClient.requestFitting(payload, { timeout: fittingTimeSet });
 	return data.result;
 };
 
