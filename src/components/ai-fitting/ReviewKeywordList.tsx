25b1023c2634077751d3e9fb1d81cf1c15723e91
brayden1102 <eyssik05@naver.com>
2026-02-11 22:31:46 +0900

    fix: remove index map key in Keyword

--- src/components/ai-fitting/ReviewKeywordList.tsx
+++ src/components/ai-fitting/ReviewKeywordList.tsx
@@ -7,9 +7,9 @@ const ReviewKeywordList = ({ keywordList }: ReviewKeywordProps) => {
 
 	return (
 		<div className="flex flex-wrap gap-2.5">
-			{keywordList.map((keyword, index) => (
+			{keywordList.map((keyword) => (
 				<div
-					key={index}
+					key={keyword}
 					className="flex justify-center items-center border border-solid border-primary-600 rounded-full px-2 py-1 whitespace-nowrap bg-white"
 				>
 					<span className="text-primary-600 text-regular-14">
